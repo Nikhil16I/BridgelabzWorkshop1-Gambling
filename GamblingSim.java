80016f935ab9cdb08667ab9d3a54f6b98f3c4f52
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-21 00:29:53 +0530

    Player's Decision to Play Or Stop Gambling

--- GamblingSim.java
+++ GamblingSim.java
@@ -2,7 +2,7 @@ package com.Workshop1;
 
 public class GamblingSim {
 	
-		//USECASE-6
+		//USECASE-7
 		final int EVERY_BET=1;     /*Bet per amount*/		
 		int STAKE=100; /*Total Stake amount*/	
 		int PLUS= 0;
@@ -15,25 +15,24 @@ public class GamblingSim {
 		int LUCKYDAY=0;int UNLUCKYDAY =0;
 				
 	
-		public void Turn () {
-			
-			//System.out.println("Welcome to the Game");		
-		int WINS = (int) (Math.random()*2);
-		if (WINS==1) {
-			STAKE+=EVERY_BET;
-			RISE++;
-			/*Player wins */ 
+public void Turn () {	
+//System.out.println("Welcome to the Game");		
+	int WINS = (int) (Math.random()*2);
+	if (WINS==1) {
+		STAKE+=EVERY_BET;
+		RISE++;
+		/*Player wins */ 
 		}
-		else {
-			STAKE-=EVERY_BET;
-			FALL++;
-			/*Player loses */ 
+	else {
+		STAKE-=EVERY_BET;
+		FALL++;
+		/*Player loses */ 
 	}
 }
 			
 public void StakeStatus() {				
-		while(STAKE>STAKEFALL && STAKE<STAKERISE ) {
-			Turn();
+	while(STAKE>STAKEFALL && STAKE<STAKERISE ) {
+		Turn();
 		}
 	if (STAKE==150) {
 		PLUS+= 50;
@@ -41,37 +40,35 @@ public void StakeStatus() {
 		System.out.println(); //SPACE
 		//System.out.println(" Your Current Stake Amount value is = $"+STAKE);
 		/*Player Loses Amount at Particular Day*/	
-	}
+	   }
 	else {
 		PLUS-= 50;
 		System.out.println("And ohh! Today YOU LOSE $50  Best luck Next time");
 		System.out.println();// SPACE
 		//System.out.println(" Your Current Stake Amount value is = $"+STAKE);
 		/*Player Wins Amount at Particular Day*/		
-
 	}
  }
 public void Monthstatus() {
-			for( int i =0; i<= 30;i++ )	{ /*Month Status*/
-				StakeStatus();
-			if(RISE>GAIN) {
-				GAIN = RISE;
-			LUCKYDAY = i+1;//Defining Lucky Status
-			}
-		    if (FALL>REDUCE) {
-		    	REDUCE=FALL;
-		    UNLUCKYDAY=	i-1;//Defining Unlucky Status
-		    }
+	for( int i =0; i<= 30;i++ )	{ /*Month Status*/
+		StakeStatus();
+	if(RISE>GAIN) {
+		GAIN = RISE;
+		LUCKYDAY = i+1;//Defining Lucky Status
+		}
+	if (FALL>REDUCE) {
+		REDUCE=FALL;
+		UNLUCKYDAY=	i-1;//Defining Unlucky Status
+		 }
 RISE =0;
 FALL =0;
-            if(PLUS>=0)
-            	System.out.println(" You Won at day " + i +" with $"+PLUS);
-            else
-            	System.out.println("You Lost at day "+i+" with $"+PLUS);
-		STAKE=100;
-						
+    if(PLUS>=0)
+    System.out.println(" You Won at day " + i +" with $"+PLUS);
+    else
+   System.out.println("You Lost at day "+i+" with $"+PLUS);
+	STAKE=100;						
    }
-	  System.out.println();//SPACE
+	 System.out.println();//SPACE
 System.out.println("Defining your Lucky Unlucky status - ");			
 System.out.println(" You won ="+GAIN+ "$ at this Day");
 System.out.println("For you Luckyday was -> DAY "+LUCKYDAY);
@@ -81,13 +78,13 @@ System.out.println(" You Lose =" +REDUCE+ "$ at this Day");
 System.out.println("For you Unluckyday was -> DAY "+UNLUCKYDAY);
    }
 public void StopORContinue() {
-int Continue =1;
-do {   /*Player can Continue if Player wins or stop Gambling on Player's Choice*/
-System.out.println("At " +Continue+ " month they are Continuing to play the Game");
-Monthstatus();
-Continue++;  //If continues
-}while (PLUS>0);
+ int Continue =1;
+ do {   /*Player can Continue if Player wins or stop Gambling on Player's Choice*/
+ System.out.println("At " +Continue+ " month they are Continuing to play the Game");
+ Monthstatus();
+ Continue++;  //If continues the Game.
+ }while (PLUS>0);
 
- }
+  }/*USECASE-7 AND Gambling Program Completed.*/
 
 }
