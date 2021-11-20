cb4e3acac28eff2a50889ef01e1448d91f1c233f
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-20 14:53:48 +0530

    Starting program for gambling by Intializing Variables and constants

---
+++ GamblingSim.java
@@ -0,0 +1,14 @@
+package com.Workshop1;
+
+public class GamblingSim {
+	
+	public static void main(String[] args) {
+		//USECASE-1
+System.out.println("Welcome to the Game");
+		int EVERY_BET=1;     /*Bet per amount*/
+		
+		int STAKE=100;       /*Total Stake amount*/	
+	}
+	  
+
+}
