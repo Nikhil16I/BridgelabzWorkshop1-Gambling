3ae0dccd81f8f17dffbf173321618f44eac6a755
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-20 14:58:18 +0530

    Checking if the player Wins or Loses the Bet

--- GamblingSim.java
+++ GamblingSim.java
@@ -3,11 +3,27 @@ package com.Workshop1;
 public class GamblingSim {
 	
 	public static void main(String[] args) {
-		//USECASE-1
+		//USECASE-2
 System.out.println("Welcome to the Game");
-		int EVERY_BET=1;     /*Bet per amount*/
-		
+		int EVERY_BET=1;     /*Bet per amount*/		
 		int STAKE=100;       /*Total Stake amount*/	
+		int WINS = (int) (Math.random()*2);
+		
+		if (WINS==1) {
+			   System.out.println("YOU LOSE = $1 dollar");
+				STAKE-=EVERY_BET;
+			   System.out.println(" Your Current Stake value is = $"+STAKE+" Dollars");
+					 /*Player loses and his current Stake Amount*/ 
+			   
+				}
+				
+				else {
+				System.out.println("YOU WIN ! = $1 dollar");
+					 STAKE+=EVERY_BET;
+				System.out.println(" Your Current Stake value is = $"+STAKE+" Dollars");
+				 /*Player wins and his current Stake Amount*/ 
+
+				}
 	}
 	  
 
