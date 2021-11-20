4905085592d23f119901fd5bb4522b75a6ac30c3
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-20 15:05:53 +0530

    Everyday Gambling Status for Player Upto 20 Days

--- GamblingSim.java
+++ GamblingSim.java
@@ -3,7 +3,7 @@ package com.Workshop1;
 public class GamblingSim {
 	
 	public static void main(String[] args) {
-		//USECASE-3
+		//USECASE-4
 System.out.println("Welcome to the Game");
 		int EVERY_BET=1;     /*Bet per amount*/		
 		int STAKE=100; /*Total Stake amount*/	
@@ -11,28 +11,39 @@ System.out.println("Welcome to the Game");
 		int STAKERISE=150; /*STAKE value if Wins*/
 		int STAKEFALL=50;  /*STAKE value if Loses*/
 		
-		int WINS = (int) (Math.random()*2);
+	
 		
+for( int i =0; i<= 20;i++ )	{	
+	STAKE=100;
+
 		while(STAKE >STAKEFALL && STAKE < STAKERISE ) {
+			int WINS = (int) (Math.random()*2);
 
 		if (WINS==1) {
-			   System.out.println("YOU LOSE = $1 dollar");
+			
 				STAKE-=EVERY_BET;
-			   System.out.println(" Your Current Stake value is = $"+STAKE+" Dollars");
 					 /*Player loses and his current Stake Amount*/ 
-			   
 				}
 				
 				else {
-				System.out.println("YOU WIN ! = $1 dollar");
 					 STAKE+=EVERY_BET;
-				System.out.println(" Your Current Stake value is = $"+STAKE+" Dollars");
 				 /*Player wins and his current Stake Amount*/ 
-
 				}
 	}
-	 System.out.println("Reached to its limit Rsiging amount with 50% Wins or loses ");
-	 System.out.println("Heres the new Amount You have = "+STAKE);
+	if (STAKE==STAKERISE) {
+		System.out.println("YOU WIN ! ");
+		System.out.println(" You Wins at day" +i);
+		System.out.println(" Your Current Stake Amount value is = $"+STAKE+" ");
+	/*Player Wins Amount at Particular Day*/	
+		
+	}
+	else {
+		System.out.println("YOU LOSE ");
+		System.out.println(" You Loses  at day "+i);
+		System.out.println(" Your Current Stake Amount value is = $"+STAKE+" ");
+		/*Player Loses Amount at Particular Day*/	
 
+	}
   }
+}
 }
