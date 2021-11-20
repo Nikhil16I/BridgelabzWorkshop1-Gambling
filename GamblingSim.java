c4da2ddc4861d35ae6b21062cef125927f457391
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-20 15:02:03 +0530

    Reached to its limit Resigning amount with 50% Wins or loses

--- GamblingSim.java
+++ GamblingSim.java
@@ -3,12 +3,18 @@ package com.Workshop1;
 public class GamblingSim {
 	
 	public static void main(String[] args) {
-		//USECASE-2
+		//USECASE-3
 System.out.println("Welcome to the Game");
 		int EVERY_BET=1;     /*Bet per amount*/		
-		int STAKE=100;       /*Total Stake amount*/	
+		int STAKE=100; /*Total Stake amount*/	
+		
+		int STAKERISE=150; /*STAKE value if Wins*/
+		int STAKEFALL=50;  /*STAKE value if Loses*/
+		
 		int WINS = (int) (Math.random()*2);
 		
+		while(STAKE >STAKEFALL && STAKE < STAKERISE ) {
+
 		if (WINS==1) {
 			   System.out.println("YOU LOSE = $1 dollar");
 				STAKE-=EVERY_BET;
@@ -25,6 +31,8 @@ System.out.println("Welcome to the Game");
 
 				}
 	}
-	  
+	 System.out.println("Reached to its limit Rsiging amount with 50% Wins or loses ");
+	 System.out.println("Heres the new Amount You have = "+STAKE);
 
+  }
 }
